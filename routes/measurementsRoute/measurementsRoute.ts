59093edde5a4f18e3e7396af47563ec5f984c1e1
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-10-23 19:43:53 +0100

    #4 measurement types swagger

--- routes/measurementsRoute/measurementsRoute.ts
+++ routes/measurementsRoute/measurementsRoute.ts
@@ -5,7 +5,37 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 let router = express.Router();
 
-router.route("/").get(async (request, result) => {
+/**
+ * @swagger
+ * /api/measurementTypes:
+ *   get:
+ *     summary: Retrieve all the measurement types
+ *     description: Retrieve all the measurement types in the system. E.g. Teaspoon, Grams, etc.
+ *     tags:
+ *       - Measurement Types
+ *     responses:
+ *       200:
+ *         description: All the measurement types in the system
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       measurementTypeID:
+ *                         type: integer
+ *                         description: The measurement type ID.
+ *                         example: 1
+ *                       measurementType:
+ *                         type: string
+ *                         description: The name of the measurement type.
+ *                         example: Teaspoon
+ */
+router.route("/measurementTypes").get(async (request, result) => {
   const allMeasurements = await prisma.measurementType.findMany();
   result.json(allMeasurements);
 });
