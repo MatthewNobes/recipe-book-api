56ceabd85194e9396c07df0e7271316c3e3aa2c2
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-10-24 20:57:10 +0100

    #6 Repointing for measurementTypesRoute

--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,11 @@
 import recipesRouter from "./recipesRoute/recipesRoute";
 import ingredientsRouter from "./ingredientsRoute/ingredientsRoute";
-import measurementsRouter from "./measurementsRoute/measurementsRoute";
+import measurementsTypesRoute from "./measurementsTypesRoute/measurementsTypesRoute";
 import express from "express";
 let router = express.Router();
 
 router.use("/recipes", recipesRouter);
 router.use("/ingredients", ingredientsRouter);
-router.use("/measurementTypes", measurementsRouter);
+router.use("/measurementTypes", measurementsTypesRoute);
 
 export default router;
