d7093699e958212d3359e8d2d8602ea7dfd5505f
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-12-28 15:08:37 +0000

    #7 removing old code

--- routes/ingredientsRoute/getIngredientsByRecipeID/getIngredientsByRecipeID.ts
+++ routes/ingredientsRoute/getIngredientsByRecipeID/getIngredientsByRecipeID.ts
@@ -39,65 +39,10 @@ export const getIngredientsByRecipeID = async (
 	});
 
 	if (recipeIngredients) {
-		console.log(recipeIngredients);
 		return recipeIngredients;
 	} else {
 		return undefined;
 	}
-
-	/**
-	const recipeIngredients = await getRecipeIngredientsByRecipeID(recipeID);
-
-	if (recipeIngredients.length === 0) {
-		return undefined;
-	}
-
-	const resultArray: FullIngredientMeasurement[] = await recipeIngredients.map(
-		async (recipeIngredient) => {
-			const fillerObject: FullIngredientMeasurement = {
-				recipeIngredientID: 45,
-				ingredientID: 1,
-				ingredient: "Egg",
-				ingredientDescription: "A chickens egg",
-				ingredientInfoURL: "https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FEgg",
-				quantity: 250,
-				measurement: "Whole",
-				recipeID: 1,
-			};
-
-			const ingredientMeasurement = await getIngredientMeasurements(
-				recipeIngredient.ingredientMeasurementID,
-			);
-			//console.log(ingredientMeasurement);
-			if (ingredientMeasurement) {
-				const ingredient = await getIngredient(
-					ingredientMeasurement.ingredientID,
-				);
-				//console.log(ingredient);
-				const measurementType = await getMeasurementType(
-					ingredientMeasurement.measurementTypeID,
-				);
-				//console.log(measurementType);
-				if (ingredient && measurementType) {
-					/**const resultObject: FullIngredientMeasurement = {
-                        recipeIngredientID: recipeIngredient.recipeIngredientsID,
-                        ingredientID: ingredient.ingredientID,
-                        ingredient: ingredient.ingredientName,
-                        ingredientDescription: ingredient.ingredientDescription,
-                        ingredientInfoURL: ingredient.ingredientInfoURL,
-                        quantity: ingredientMeasurement.measurementSize,
-                        measurement: measurementType.measurementType,
-                        recipeID: recipeIngredient.recipeID,
-                    };
-
-					return fillerObject;
-				}
-			}
-			return fillerObject;
-		},
-	);
-	return resultArray;
-     */
 };
 
 export const getRecipeIngredientsByRecipeID = async (
